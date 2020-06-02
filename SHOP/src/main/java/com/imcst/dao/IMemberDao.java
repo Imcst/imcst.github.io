d6e95906cdae3b0affb9a5cce4997a692546135e
admin <imcst@foxmail.com>
2020-06-02 15:32:18 +0800

    更新

--- SHOP/src/main/java/com/imcst/dao/IMemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.imcst.dao;
-
-import com.imcst.domain.Member;
-import org.apache.ibatis.annotations.Select;
-
-public interface IMemberDao {
-    @Select("select * from member where id =#{id}")
-     public Member findById(String id) throws Exception;
-}
